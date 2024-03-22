10a6446a058db7314636ff9fe306a47ffcc7c9ef
arensin8 <arensinani4@gmail.com>
2024-03-22 19:46:12 +0400

    Creating chat app using redis

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const socketIO = require("socket.io");
-const redis = require("redis");
-const redisClient = redis.createClient();
+const SocketIO = require("socket.io");
+const { createClient } = require("redis");
+const redisClient = createClient();
 app.set("view engine", "ejs");
 const server = http.createServer(app);
-const io = socketIO(server, { cors: { origin: "*" } });
 const PORT = 3000;
-
-function sendMessages(socket) {
-  redisClient.lRange("messages", "0", "-1", (err, data) => {
-    console.log(data);
+const io = SocketIO(server, { cors: { origin: "*" } });
+async function sendMessages(socket) {
+  redisClient.lrange("messages", 0, -1, (err, data) => {
     data.map((item) => {
       const [username, message] = item.split(":");
       socket.emit("message", {
@@ -21,24 +19,20 @@ function sendMessages(socket) {
     });
   });
 }
-
-io.on("connection", (socket) => {
-  const redisMulti = redisClient.multi();
-  socket.on("message", ({ username, message }) => {
-    redisMulti.rPush("messages", `${username}:${message}`);
+io.on("connection", async (socket) => {
+  sendMessages(socket);
+  socket.on("message", async ({ username, message }) => {
+    redisClient.rpush("messages", `${username}:${message}`);
     io.emit("message", { username, message });
   });
 });
-
+app.get("/", (req, res) => {
+  res.render("login");
+});
 app.get("/chat", (req, res) => {
   const { username } = req.query;
   res.render("chat", { username });
 });
-
-app.get("/", (req, res) => {
-  res.render("login");
-});
-
 server.listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`);
+  console.log(`Server Run At ${PORT}`);
 });
